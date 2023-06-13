b15b3ff5f10c354fda0cacb80566a7aa7363bde6
Markim-Becker <markimbecker47@gmail.com>
2023-06-13 17:24:30 +0200

    changes

---
+++ helloWolrd.py
@@ -0,0 +1,7 @@
+# changed the code to take input and then print that input
+user_string = input('\nPlease enter a sentance: ')
+
+print('\n',user_string)
+
+
+# bla blah blah blah
---
+++ python-helloWolrd.py
@@ -0,0 +1,10 @@
+# changed the code to take input and then print that input
+user_string = input('\nPlease enter a sentance: ')
+
+# blah blah blah blah
+print('\n',user_string)
+
+
+# bla blah blah blah
+
+#more blah blah blah
